4c2b3806e6401e2bab24ea63cc419a0ab3379d2d
GianlucaGrammatica <grammaticagianluca@gmail.com>
2026-05-12 23:42:42 +0200

    Pulizia del codice

--- src/main.cpp
+++ src/main.cpp
@@ -8,86 +8,68 @@
 #include <ArduinoJson.h>
 #include "config.h"
 
-// TODO: quando arrivano i sensori, aggiungere le librerie qui
-// #include <DHT.h>
-// #define DHT_PIN D4
-// #define DHT_TYPE DHT22
-// DHT dht(DHT_PIN, DHT_TYPE);
-
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-
 #define LED_PIN D5
 #define BTN_PIN D6
 
-#define SENSOR_INTERVAL  60000UL // pubblica sensori ogni 60s
-#define PING_INTERVAL    30000UL // ping online ogni 30s
-
-// Setup Display
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
-// Setup WiFi & MQTT
 WiFiClientSecure espClient;
-PubSubClient client(espClient);
+PubSubClient mqttClient(espClient);
 
-// Timer
+// --- STRUCTS ---
+struct PlantConfig {
+    String name = "Waiting...";
+    float humMin = 0, humMax = 100;
+    float tempMin = 0, tempMax = 50;
+    float soilHumMin = 0, soilHumMax = 100;
+};
+
+struct SensorReadings {
+    float humidity = 0;
+    float temperature = 0;
+    float soilHum = 0;
+    float luminosity = 0;
+};
+
+PlantConfig currentConfig;
+SensorReadings currentReadings;
+
+// --- TIMERS ---
 unsigned long lastSensorPublish = 0;
-unsigned long lastPing          = 0;
+unsigned long lastDisplayUpdate = 0;
+unsigned long lastDebounceTime = 0;
+bool lastButtonState = HIGH;
 
-// Condizioni ottimali ricevute dal server
-String plantName   = "In attesa...";
-float  humMin      = 0,   humMax     = 100;
-float  tempMin     = 0,   tempMax    = 50;
-float  soilHumMin  = 0,   soilHumMax = 100;
 
-// Ultime letture sensori
-float curHumidity    = 0;
-float curTemperature = 0;
-float curSoilHum     = 0;
-
-// ---------------------------------------------------------------
-
-void drawDisplay(bool online) {
+void updateOLED(bool isOnline) {
     display.clearDisplay();
+    display.setTextColor(SSD1306_WHITE);
+    display.setTextSize(1);
 
-    // Nome piana e stato connessione
+    display.clearDisplay();
     display.setTextSize(1);
     display.setCursor(0, 0);
-    String name = plantName.length() > 12 ? plantName.substring(0, 11) + "." : plantName;
-    display.print(name);
 
-    display.setCursor(128 - (5 * 6), 0);
-    display.print(online ? "* ONL" : "  OFF");
+    // Taglia il nome se è troppo lungo
+    String displayName = currentConfig.name.length() > 12 ? currentConfig.name.substring(0, 11) + "." : currentConfig.name;
+    display.print(displayName);
+    display.setCursor(100, 0);
+    display.print(isOnline ? "ON" : "OFF");
 
-    // Linea
     display.drawLine(0, 10, 128, 10, SSD1306_WHITE);
 
-    // Controllo range ottimali
-    bool tempOk = (curTemperature >= tempMin && curTemperature <= tempMax);
-    bool humOk  = (curHumidity   >= humMin  && curHumidity   <= humMax);
-    bool soilOk = (curSoilHum    >= soilHumMin && curSoilHum <= soilHumMax);
-
     display.setCursor(0, 14);
-    display.print("Temp: "); display.print(curTemperature, 1); display.print("C  ");
-    display.println(tempOk ? "OK" : "!!");
-
+    display.printf("Temp: %.1f C\n", currentReadings.temperature);
     display.setCursor(0, 26);
-    display.print("Hum:  "); display.print(curHumidity, 1); display.print("%  ");
-    display.println(humOk ? "OK" : "!!");
-
+    display.printf("Hum:  %.1f %%\n", currentReadings.humidity);
     display.setCursor(0, 38);
-    display.print("Soil: "); display.print(curSoilHum, 1); display.print("%  ");
-    display.println(soilOk ? "OK" : "!!");
-
-    // Faccina stato generale
-    bool allOk = tempOk && humOk && soilOk;
-    display.setCursor(104, 52);
-    display.print(allOk ? ":)" : ":(");
+    display.printf("Soil: %.1f %%\n", currentReadings.soilHum);
 
     display.display();
 }
 
-void setup_wifi() {
+void setupWiFi() {
     display.clearDisplay();
     display.setCursor(0, 10);
     display.println("Connecting WiFi...");
@@ -98,172 +80,115 @@ void setup_wifi() {
         delay(500);
         Serial.print(".");
     }
-    Serial.println("\nWiFi connesso!");
+    Serial.println("\nWiFi connesso");
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-    String incomingMsg;
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+    String message = "";
     for (unsigned int i = 0; i < length; i++) {
-        incomingMsg += (char)payload[i];
+        message += (char)payload[i];
     }
-
-    Serial.println("Topic: " + String(topic));
-    Serial.println("Payload: " + incomingMsg);
 
     String topicStr = String(topic);
 
-    // Comandi semplici (LED, ecc.)
+    // Toggle LED
     if (topicStr == String("device/") + DEVICE_TOKEN) {
-        if (incomingMsg == "ON") {
-            digitalWrite(LED_PIN, HIGH);
-        } else if (incomingMsg == "OFF") {
-            digitalWrite(LED_PIN, LOW);
-        }
-        return;
+        if (message == "ON") digitalWrite(LED_PIN, HIGH);
+        else if (message == "OFF") digitalWrite(LED_PIN, LOW);
     }
 
-    // Configurazione pianta
+    // Parsing della configurazione
     if (topicStr == String("device/") + DEVICE_TOKEN + "/config") {
         StaticJsonDocument<512> doc;
-        DeserializationError err = deserializeJson(doc, incomingMsg);
+        DeserializationError err = deserializeJson(doc, message);
+        if (!err) {
+            currentConfig.name = doc["plant_name"].as<String>();
+            currentConfig.humMin = doc["hum_min"] | 0.0f;
+            currentConfig.humMax = doc["hum_max"] | 100.0f;
+            currentConfig.tempMin = doc["temp_min"] | 0.0f;
+            currentConfig.tempMax = doc["temp_max"] | 50.0f;
+            currentConfig.soilHumMin = doc["soil_hum_min"] | 0.0f;
+            currentConfig.soilHumMax = doc["soil_hum_max"] | 100.0f;
 
-        if (err) {
-            Serial.println("Errore JSON config: " + String(err.c_str()));
-            return;
+            updateOLED(mqttClient.connected());
         }
-
-        plantName  = doc["plant_name"].as<String>();
-        humMin     = doc["hum_min"]     | 0.0f;
-        humMax     = doc["hum_max"]     | 100.0f;
-        tempMin    = doc["temp_min"]    | 0.0f;
-        tempMax    = doc["temp_max"]    | 50.0f;
-        soilHumMin = doc["soil_hum_min"] | 0.0f;
-        soilHumMax = doc["soil_hum_max"] | 100.0f;
-
-        Serial.println("Config ricevuta: " + plantName);
-        drawDisplay(true);
-        return;
     }
 }
 
-void reconnect() {
-    while (!client.connected()) {
-        display.clearDisplay();
-        display.setCursor(0, 10);
-        display.println("Connecting MQTT...");
-        display.display();
-
+void connectMQTT() {
+    if (!mqttClient.connected()) {
         String clientId = String("ESP-") + DEVICE_TOKEN;
-
-        if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASS)) {
-            client.subscribe((String("device/") + DEVICE_TOKEN).c_str(), 1);
-            client.subscribe((String("device/") + DEVICE_TOKEN + "/config").c_str(), 1);
-
-            Serial.println("MQTT connesso!");
-
-            // Ping connessione iniziale
-            client.publish(
-                (String("device/") + DEVICE_TOKEN + "/status").c_str(),
-                "ONLINE"
-            );
-        } else {
-            Serial.print("MQTT fallito, rc=");
-            Serial.println(client.state());
-            delay(5000);
+        if (mqttClient.connect(clientId.c_str(), MQTT_USER, MQTT_PASS)) {
+            mqttClient.subscribe((String("device/") + DEVICE_TOKEN).c_str(), 1);
+            mqttClient.subscribe((String("device/") + DEVICE_TOKEN + "/config").c_str(), 1);
+            mqttClient.publish((String("device/") + DEVICE_TOKEN + "/status").c_str(), "ONLINE");
+            mqttClient.subscribe((String("device/") + DEVICE_TOKEN + "/updates").c_str(), 1);
         }
     }
 }
 
-void publishPing() {
-    if (millis() - lastPing < PING_INTERVAL) return;
-    lastPing = millis();
+void handleButton() {
+    bool currentButtonState = digitalRead(BTN_PIN);
 
-    client.publish(
-        (String("device/") + DEVICE_TOKEN + "/status").c_str(),
-        "ONLINE"
-    );
+    // Controlla se il bottone è appena stato premuto (da HIGH a LOW)
+    if (lastButtonState == HIGH && currentButtonState == LOW) {
+
+        // Debounce accetta il click solo se sono passati 200ms dall'ultimo
+        if (millis() - lastDebounceTime > 200) {
+            mqttClient.publish((String("device/") + DEVICE_TOKEN + "/updates").c_str(), "BUTTON_PRESSED");            Serial.println("Click inviato!");
+            lastDebounceTime = millis();
+        }
+    }
+
+    lastButtonState = currentButtonState;
 }
 
-void publishSensorData() {
-    if (millis() - lastSensorPublish < SENSOR_INTERVAL) return;
-    lastSensorPublish = millis();
-
-    // TODO: sostituire con letture reali quando arrivano i sensori
-    // curHumidity    = dht.readHumidity();
-    // curTemperature = dht.readTemperature();
-    // curSoilHum     = map(analogRead(SOIL_PIN), 0, 1023, 100, 0);
-    float luminosity = 0.0;
-
-    String payload = "{";
-    payload += "\"type\":\"sensor_data\",";
-    payload += "\"humidity\":"      + String(curHumidity,    1) + ",";
-    payload += "\"temperature\":"   + String(curTemperature, 1) + ",";
-    payload += "\"soil_humidity\":" + String(curSoilHum,     1) + ",";
-    payload += "\"luminosity\":"    + String(luminosity,     1);
-    payload += "}";
-
-    client.publish(
-        (String("device/") + DEVICE_TOKEN + "/updates").c_str(),
-        payload.c_str()
-    );
-
-    Serial.println("Sensori inviati: " + payload);
+void readSensors() {
+    // TO-DO Lettura dei sonsori
 }
 
-// ---------------------------------------------------------------
+void publishTelemetry() {
+    // Ogni 60 secondi
+    if (millis() - lastSensorPublish > 60000) {
+        lastSensorPublish = millis();
+
+        String payload = "{";
+        payload += "\"type\":\"sensor_data\",";
+        payload += "\"humidity\":" + String(currentReadings.humidity, 1) + ",";
+        payload += "\"temperature\":" + String(currentReadings.temperature, 1) + ",";
+        payload += "\"soil_humidity\":" + String(currentReadings.soilHum, 1) + ",";
+        payload += "\"luminosity\":" + String(currentReadings.luminosity, 1);
+        payload += "}";
+
+        mqttClient.publish((String("device/") + DEVICE_TOKEN + "/updates").c_str(), payload.c_str());    }
+}
+
 
 void setup() {
     Serial.begin(115200);
-
     pinMode(LED_PIN, OUTPUT);
     pinMode(BTN_PIN, INPUT_PULLUP);
 
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-        Serial.println("Display OLED non trovato");
-        for (;;);
-    }
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-
-    display.clearDisplay();
-    display.setCursor(0, 10);
-    display.println("NaHida Starting...");
-    display.display();
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
     espClient.setInsecure();
-    setup_wifi();
+    setupWiFi();
 
-    client.setServer(MQTT_SERVER, MQTT_PORT);
-    client.setCallback(callback);
+    mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+    mqttClient.setCallback(mqttCallback);
 }
 
 void loop() {
-    if (!client.connected()) {
-        reconnect();
-    }
-    client.loop();
+    connectMQTT();
+    mqttClient.loop();
 
-    // Bottone fisico test
-    static bool lastBtnState = HIGH;
-    bool currentBtnState = digitalRead(BTN_PIN);
+    handleButton();
+    readSensors();
+    publishTelemetry();
 
-    if (lastBtnState == HIGH && currentBtnState == LOW) {
-        client.publish(
-            (String("device/") + DEVICE_TOKEN + "/updates").c_str(),
-            "BUTTON_PRESSED"
-        );
-        Serial.println("BUTTON_PRESSED inviato");
-        delay(200);
-    }
-    lastBtnState = currentBtnState;
-
-    publishPing();
-    publishSensorData();
-
-    // Aggiorna display ogni 5 secondi
-    static unsigned long lastDraw = 0;
-    if (millis() - lastDraw > 5000) {
-        lastDraw = millis();
-        drawDisplay(client.connected());
+    // Aggiornamento scehrmo ogni 2 secondi
+    if (millis() - lastDisplayUpdate > 2000) {
+        lastDisplayUpdate = millis();
+        updateOLED(mqttClient.connected());
     }
 }
